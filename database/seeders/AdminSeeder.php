3c230cd7bfe22e42deea5a8c40b0d8aad5464436
S M Ariq Islam <arikislam321@gmail.com>
2022-06-28 16:37:57 +0600

    Admoin seeder added

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Hash;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'email' => 'admin@admin.com',
+            'name' => 'Admin',
+            'is_admin' => true,
+             'password' => Hash::make('password')
+        ];
+
+        User::create($data);
+
+    }
+}
